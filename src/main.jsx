0eb05c1da3872f9421e58ddea1ee543fb03b5d95
IlyaKrasulia <ilyakras97@gmail.com>
2026-02-25 23:57:59 +0200

    feat: add create manual order screen

--- src/main.jsx
+++ src/main.jsx
@@ -4,15 +4,26 @@ import { createRoot } from "react-dom/client";
 
 import "./index.css";
 import { AppLayout } from "./components/AppLayout";
+import { CreateManualOrder } from "./pages/mainStack/CreateManualOrder";
 import { ImportCSVScreen } from "./pages/mainStack/ImportCSV";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <AppLayout
+
+    {/* Import CSV Flow */}
+
+    {/* <AppLayout
       title="Import Orders"
       info="Upload your sales data to calculate taxes and generate reports automatically."
     >
       <ImportCSVScreen />
+    </AppLayout> */}
+
+    <AppLayout
+      title="Create Order Manually"
+      info="Enter the location coordinates and order amount to calculate taxes and save the entry."
+    >
+      <CreateManualOrder />
     </AppLayout>
   </StrictMode>,
 );
