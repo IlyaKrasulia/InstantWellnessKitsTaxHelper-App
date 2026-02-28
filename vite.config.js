3e2f110cba21428912618ee186512726e47c117a
Ілля Красуля <illakrasula@MacBook-Pro-Illa.local>
2026-02-28 21:56:34 +0200

    fix: change dev port

--- vite.config.js
+++ vite.config.js
@@ -9,6 +9,10 @@ const __dirname = path.dirname(fileURLToPath(import.meta.url))
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: 5045,
+    strictPort: true,
+  },
   base: '/',
   resolve: {
     alias: {
